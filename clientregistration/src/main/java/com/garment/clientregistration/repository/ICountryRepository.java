bbd9e7cd3a135b14f9e61c5b0d14b46e50eede41
SHEIKH SAJIB <sheikhsajib99@gmail.com>
2023-01-22 01:05:40 +0600

    Production Line Primary Stage

--- clientregistration/src/main/java/com/garment/clientregistration/repository/ICountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.garment.clientregistration.repository;
-
-import com.garment.clientregistration.entity.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICountryRepository extends JpaRepository<Country, Integer> {
-}
